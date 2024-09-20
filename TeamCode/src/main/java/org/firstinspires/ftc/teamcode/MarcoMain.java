a71026ea38507d665c131c1a61eef2ca12116887
m4zt <marcogough07@gmail.com>
2024-09-20 14:09:15 -0400

    experimental code that can run the abstract function DriveTrainFunctions and use the sub functions inside of it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarcoMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarcoMain.java
@@ -1,10 +1,4 @@
-
-
 package org.firstinspires.ftc.teamcode;
-
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -12,7 +6,57 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 abstract class driveTrainFunctions {
+    abstract double[] driveTrainMath(double left_stick_y, double left_stick_x, double right_stick_x);
+}
 
+class driveFunctions extends driveTrainFunctions {
+    double[] driveTrainMath(double left_stick_y, double left_stick_x, double right_stick_x) {
+        double max;
+
+        // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+        // Note: pushing stick forward gives negative value (-left_stick_y) = forward
+        // Possible future change: replace game-pad inputs with inputs into function for
+        // centralized adjustment of controls
+
+        // Combine the joystick requests for each axis-motion to determine each wheel's power.
+        // Set up a variable for each drive wheel to save the power level for telemetry.
+        double leftFrontPower  = -left_stick_y + left_stick_x + right_stick_x;
+        double rightFrontPower = -left_stick_y - left_stick_x - right_stick_x;
+        double leftBackPower   = -left_stick_y - left_stick_x + right_stick_x;
+        double rightBackPower  = -left_stick_y + left_stick_x - right_stick_x;
+
+        // Normalize the values so no wheel power exceeds 100%
+        // This ensures that the robot maintains the desired motion.
+        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower  /= max;
+            rightFrontPower /= max;
+            leftBackPower   /= max;
+            rightBackPower  /= max;
+        }
+
+        // Send calculated power to wheels
+        // leftFrontDrive.setPower(leftFrontPower);
+        // rightFrontDrive.setPower(rightFrontPower);
+        // leftBackDrive.setPower(leftBackPower);
+        // rightBackDrive.setPower(rightBackPower);
+
+        double[] motorPower;
+        motorPower = new double[4];
+        motorPower[0] = leftFrontPower;
+        motorPower[1] = rightFrontPower;
+        motorPower[2] = leftBackPower;
+        motorPower[3] = rightBackPower;
+
+        return motorPower;
+    }
+}
+
+@TeleOp()
+public class MarcoMain extends LinearOpMode {
     // Declare OpMode members for each of the 4 motors.
     public ElapsedTime runtime = new ElapsedTime();
     public DcMotor leftFrontDrive = null;
@@ -20,16 +64,10 @@ abstract class driveTrainFunctions {
     public DcMotor rightFrontDrive = null;
     public DcMotor rightBackDrive = null;
 
-    abstract void initdriveTrain();
-    abstract void updatedriveTrain();
-}
+    driveTrainFunctions drivetrainFunctions = new driveFunctions();
 
-class driveFunctions extends driveTrainFunctions {
-
-    // Copy of the functions from DriveTrainFunctions.java
-    void initdriveTrain() {
-        runtime.reset();
-
+    @Override
+    public void runOpMode() {
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
         leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
@@ -46,65 +84,28 @@ class driveFunctions extends driveTrainFunctions {
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-    }
 
-    void updatedriveTrain() {
-        double max;
-
-        // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-        double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-        double lateral =  gamepad1.left_stick_x;
-        double yaw     =  gamepad1.right_stick_x;
-        // Possible future change: replace game-pad inputs with inputs into function for
-        // centralized adjustment of controls
-
-        // Combine the joystick requests for each axis-motion to determine each wheel's power.
-        // Set up a variable for each drive wheel to save the power level for telemetry.
-        double leftFrontPower  = axial + lateral + yaw;
-        double rightFrontPower = axial - lateral - yaw;
-        double leftBackPower   = axial - lateral + yaw;
-        double rightBackPower  = axial + lateral - yaw;
-
-        // Normalize the values so no wheel power exceeds 100%
-        // This ensures that the robot maintains the desired motion.
-        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-        max = Math.max(max, Math.abs(leftBackPower));
-        max = Math.max(max, Math.abs(rightBackPower));
-
-        if (max > 1.0) {
-            leftFrontPower  /= max;
-            rightFrontPower /= max;
-            leftBackPower   /= max;
-            rightBackPower  /= max;
-        }
-
-        // Send calculated power to wheels
-        leftFrontDrive.setPower(leftFrontPower);
-        rightFrontDrive.setPower(rightFrontPower);
-        leftBackDrive.setPower(leftBackPower);
-        rightBackDrive.setPower(rightBackPower);
-
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-        telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-        telemetry.update();
-    }
-}
-
-@TeleOp()
-public class MarcoMain extends LinearOpMode {
-    driveTrainFunctions drivetrainFunctions = new driveFunctions();
-
-    @Override
-    public void runOpMode() throws InterruptedException {
         waitForStart();
-        drivetrainFunctions.initdriveTrain();
+        runtime.reset();
 
-        if (opModeIsActive()) {
-            while (opModeIsActive()) {
-                drivetrainFunctions.updatedriveTrain();
-            }
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            double[] motorPower = drivetrainFunctions.driveTrainMath(
+                    gamepad1.left_stick_y,
+                    gamepad1.left_stick_x,
+                    gamepad1.right_stick_x
+            );
+            // Send calculated power to wheels
+            leftFrontDrive.setPower(motorPower[0]);
+            rightFrontDrive.setPower(motorPower[1]);
+            leftBackDrive.setPower(motorPower[2]);
+            rightBackDrive.setPower(motorPower[3]);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", motorPower[0], motorPower[1]);
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", motorPower[2], motorPower[3]);
+            telemetry.update();
         }
     }
 }
