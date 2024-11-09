54e2f6d18e003c16310392da781b97b3b8e700f7
m4zt <marcogough07@gmail.com>
2024-11-09 11:03:59 -0500

    prototype turning for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Paths/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Paths/Test.java
@@ -10,7 +10,7 @@ import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
 @Config
 @Autonomous(name = "Move Left")
-public abstract class Test extends OpMode {
+/* public abstract class Test extends OpMode {
     private Follower follower;
     private Path left;
 
@@ -20,6 +20,20 @@ public abstract class Test extends OpMode {
 
         left = new Path(new BezierLine(new Point(0, 0, Point.CARTESIAN), new Point(0, 40, Point.CARTESIAN)));
         left.setConstantHeadingInterpolation(0);
+
+        follower.followPath(left);
+    }
+} */
+public abstract class Test extends OpMode {
+    private Follower follower;
+    private Path left;
+
+    @Override
+    public void init() {
+        follower = new Follower(hardwareMap);
+
+        left = new Path(new BezierLine(new Point(0, 0, Point.CARTESIAN), new Point(0, 40, Point.CARTESIAN)));
+        left.setConstantHeadingInterpolation(90);
 
         follower.followPath(left);
     }
