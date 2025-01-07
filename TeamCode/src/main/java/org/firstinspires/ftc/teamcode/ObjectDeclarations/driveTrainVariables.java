4de726e0bee424628036377c35908f28094feb92
m4zt <marcogough07@gmail.com>
2025-01-07 14:40:48 -0500

    added untested acceleration to mecanumfunctions, ill test on wednesday

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectDeclarations/driveTrainVariables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectDeclarations/driveTrainVariables.java
@@ -5,5 +5,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class driveTrainVariables {
     public static DcMotor[] driveTrainMotorPower = new DcMotor[4];
     public static final double driveTrainDefaultMaxPower = 0.8;
+    public static final double acceleration_rate = 0.05;
     public static double driveTrainMaxPower = driveTrainDefaultMaxPower;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/MecanumFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/MecanumFunctions.java
@@ -13,25 +13,47 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 // Call to update current motor power
 public class MecanumFunctions {
         private Follower follower;
+        private static double[] oldDriveTrainMotorPowers = new double[driveTrainVariables.driveTrainMotorPower.length];
 
         public void init(HardwareMap hardwareMap) {
             follower = new Follower(hardwareMap);
             follower.startTeleopDrive();
         }
+
+        public void updateOldMotorPowers() {
+            for (int i = 0; i < driveTrainVariables.driveTrainMotorPower.length; i++) {
+                oldDriveTrainMotorPowers[i] = driveTrainVariables.driveTrainMotorPower[i].getPower();
+            }
+        }
         public void updateTeleOpMovement(Gamepad gamepad1) {
+            updateOldMotorPowers();
+
             follower.setTeleOpMovementVectors(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x);
             follower.update();
 
-            for (int i = 0; i < driveTrainVariables.driveTrainMotorPower.length; i++) {
+            /*for (int i = 0; i < driveTrainVariables.driveTrainMotorPower.length; i++) {
                 if (driveTrainVariables.driveTrainMotorPower[i].getPower() > driveTrainVariables.driveTrainMaxPower) {
                     driveTrainVariables.driveTrainMotorPower[i].setPower(driveTrainVariables.driveTrainMaxPower);
                 } else if (driveTrainVariables.driveTrainMotorPower[i].getPower() < -driveTrainVariables.driveTrainMaxPower) {
                     driveTrainVariables.driveTrainMotorPower[i].setPower(-driveTrainVariables.driveTrainMaxPower);
                 }
-            }
+            }*/
 
-            // Telemetry data
-            // telemetry.addData("Front left/Right", "%4.2f, %4.2f", driveTrainVariables.driveTrainMotorPower[0], driveTrainVariables.driveTrainMotorPower[1]);
-            // telemetry.addData("Back  left/Right", "%4.2f, %4.2f", driveTrainVariables.driveTrainMotorPower[2], driveTrainVariables.driveTrainMotorPower[3]);
+            for (int i = 0; i < driveTrainVariables.driveTrainMotorPower.length; i++) {
+                double targetPower = driveTrainVariables.driveTrainMotorPower[i].getPower();
+                double currentPower = oldDriveTrainMotorPowers[i];
+                double powerDifference = targetPower - currentPower;
+
+                if (powerDifference > driveTrainVariables.acceleration_rate) {
+                    oldDriveTrainMotorPowers[i] += driveTrainVariables.acceleration_rate;
+                } else if (powerDifference < -driveTrainVariables.acceleration_rate) {
+                    oldDriveTrainMotorPowers[i] -= driveTrainVariables.acceleration_rate;
+                } else {
+                    oldDriveTrainMotorPowers[i] = targetPower;
+                }
+
+                double adjustedPower = Math.max( -driveTrainVariables.driveTrainMaxPower, Math.min( driveTrainVariables.driveTrainMaxPower, oldDriveTrainMotorPowers[i] ) );
+                driveTrainVariables.driveTrainMotorPower[i].setPower(adjustedPower);
+            }
         }
     }
