1e4bfbfa1c4363eaa7fdf5ab882fba5784217f08
JosephVenech <163907724+JosephVenech@users.noreply.github.com>
2025-01-29 10:46:04 -0500

    Fixed localization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/ThreeWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/ThreeWheelLocalizer.java
@@ -55,10 +55,9 @@ public class ThreeWheelLocalizer extends Localizer {
     private Pose rightEncoderPose;
     private Pose strafeEncoderPose;
     private double totalHeading;
-    public static double FORWARD_TICKS_TO_INCHES = 0.00138;//8192 * 1.37795 * 2 * Math.PI * 0.5008239963;
-    public static double STRAFE_TICKS_TO_INCHES = 0.0013;//8192 * 1.37795 * 2 * Math.PI * 0.5018874659;
-    public static double TURN_TICKS_TO_RADIANS = .00216;//8192 * 1.37795 * 2 * Math.PI * 0.5;
-    //Joseph -- Since negative may need to reverse some encoders
+    public static double FORWARD_TICKS_TO_INCHES = (.0013+.0013+.0013+.0013+.0013) / 5;
+    public static double STRAFE_TICKS_TO_INCHES = (-0.0013-0.0013-.0013-.0013-.0013) / 5;
+    public static double TURN_TICKS_TO_RADIANS = (.0016+.0017+.0017+.0017+.0017) / 5;
 
     /**
      * This creates a new ThreeWheelLocalizer from a HardwareMap, with a starting Pose at (0,0)
